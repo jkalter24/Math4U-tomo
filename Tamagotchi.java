ae776428be8c762ebd1fe9d465b3c1683abe2f99
jkalter24 <70232140+jkalter24@users.noreply.github.com>
2023-10-10 13:00:58 -0400

    Update Tamagotchi.java

--- Tamagotchi.java
+++ Tamagotchi.java
@@ -26,6 +26,10 @@ public class Tamagotchi {
         feedButton = new JButton("Feed");
         playButton = new JButton("Play");
         sleepButton = new JButton("Sleep");
+        // Sets the icon to the image
+        ImageIcon img = new ImageIcon(/img/icon.png);
+        // Set the Icon image
+        myFrame.setIconImage(img.getImage());
 
         // Add action listeners to the buttons
         feedButton.addActionListener(e -> feed());
